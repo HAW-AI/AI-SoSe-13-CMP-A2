55353291e08241c6e7807ae0653fb5ffbcd1933e
Patrick Detlefsen <patrick.detlefsen@gmail.com>
2013-05-01 18:52:36 +0200

    removed unused class

--- src/haw/ai/komponenten/lager_komponente/ProduktBusinessLogik.java
+++
@@ -1,7 +0,0 @@
-package haw.ai.komponenten.lager_komponente;
-
-import haw.ai.komponenten.common.KomponentenBusinessLogik;
-
-public class ProduktBusinessLogik implements KomponentenBusinessLogik {
-
-}
