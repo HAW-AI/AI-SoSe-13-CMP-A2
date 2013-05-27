8c571c4bf1e8bbf566a27f78505e54da5bbc02f7
Patrick Detlefsen <patrick.detlefsen@gmail.com>
2013-05-27 21:21:10 +0200

    ignore dist/

--- dist/client.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-java -Djava.security.manager -Djava.security.policy=the.policy -Djava.rmi.server.codebase='file://client.jar file://lib/' -jar client.jar
--- dist/server.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-java -Djava.security.manager -Djava.security.policy=the.policy -Djava.rmi.server.codebase='file://server.jar file://lib/' -jar server.jar
